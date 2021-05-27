Softmax error fixed

main.py
@@ -88,9 +88,9 @@ def main(args):
         val_loader   = torch.utils.data.DataLoader(val_set, batch_size=args.batch_size, shuffle=True, num_workers=2)
     else:
         
-        train_set    = build_mprage(root='/scratch/mplatscher/imaging_data/', train=True, train_size=0.8, transform=None)
+        train_set    = build_mprage(root='/scratch/platscher/imaging_data/', train=True, train_size=0.8, transform=None)
         train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=2)
-        val_set      = build_mprage(root='/scratch/mplatscher/imaging_data/', train=False, train_size=0.8, transform=None)
+        val_set      = build_mprage(root='/scratch/platscher/imaging_data/', train=False, train_size=0.8, transform=None)
         val_loader   = torch.utils.data.DataLoader(val_set, batch_size=args.batch_size, shuffle=False, num_workers=2)
         
 

metrics.py
@@ -5,10 +5,10 @@ from einops import rearrange
 
 def dice(prediction, target, train=True, classes=2, eps=1E-6):
 
-    # if train:
-    #     prediction = torch.softmax(prediction, dim=1)
-    # else:
-    if not train
+    #if train:
+    #    prediction = torch.softmax(prediction, dim=1)
+    #else:
+    if not train:
         prediction = torch.argmax(prediction, dim=1)
         prediction = F.one_hot(prediction, num_classes=classes)
         if len(prediction.shape) == 4: